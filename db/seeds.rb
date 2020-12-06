f602e081d1d736da8a63f1fbab863fbbb67b4fd8
Smrity <baralsmrity8@gmail.com>
2020-12-06 15:42:32 +0700

    Admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+AdminUser.create!(email: 'admin@admin.com', password: 'loginnow', password_confirmation: 'loginnow') if Rails.env.development?
